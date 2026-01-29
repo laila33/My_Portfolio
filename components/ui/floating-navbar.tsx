81eb09b5e3173660cfcde43abe4a587213c05591
laila33 <lailaabdelnaser64@gmail.com>
2026-01-29 15:46:23 +0200

    last

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -8,7 +8,6 @@ import {
 } from "motion/react";
 import { cn } from "@/lib/utils";
 
-
 export const FloatingNav = ({
   navItems,
   className,
@@ -55,8 +54,10 @@ export const FloatingNav = ({
         transition={{
           duration: 0.2,
         }}
+        //  bg-purple-600/40 border-white/[0.2]
         className={cn(
-"hidden sm:flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 items-center justify-center space-x-4 border-white/[0.2]",          className
+          "hidden sm:flex max-w-fit fixed top-10 inset-x-0 mx-auto border font-bold border-purple-950  text-purple-300 rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 items-center justify-center space-x-4 ",
+          className,
         )}
       >
         {navItems.map((navItem: any, idx: number) => (
@@ -64,14 +65,13 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+              "relative items-center flex space-x-1 text-purple-600 dark:hover:text-neutral-300 hover:text-neutral-500",
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
             <span className="hidden sm:block text-sm">{navItem.name}</span>
           </a>
         ))}
-   
       </motion.div>
     </AnimatePresence>
   );
